61508c6e4320c07af3f3ea6d0d6fe26cce46398f
marcosh17 <m.marcosh18@gmail.com>
2025-07-06 22:40:22 +0200

    refactor(config): load lat/lon from imported location dictionary

--- app/config.py
+++ app/config.py
@@ -1,9 +1,15 @@
 from dotenv import load_dotenv
 import os
+from data.locations import LOCATIONS  
 
-load_dotenv()  
+load_dotenv()
 
+# Token API REE
 ESIOS_TOKEN = os.getenv("ESIOS_TOKEN")
 
 if not ESIOS_TOKEN:
     raise EnvironmentError("ESIOS_TOKEN is not set in the .env file")
+
+DEFAULT_LOCATION = "madrid"
+LATITUDE = LOCATIONS[DEFAULT_LOCATION]["lat"]
+LONGITUDE = LOCATIONS[DEFAULT_LOCATION]["lon"]
